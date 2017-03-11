34f6b142bff452d95510828dc6a3ebe83d43b83f
chengran02 <chengran02@126.com>
2017-03-11 13:09:16 +0800

    Add files via upload
    Dear professor, this is my level test results that may be a little bit ugly since I a newer in studying R.

--- test_one.R
+++ test_one.R
@@ -1,2 +1,11 @@
-test one
-time: two days
+findNum <- function(y,x,I){
+  a <- NULL
+  n <- length(y)
+  for(i in 1:(n-I+1)){
+     if(all(y[i:(i+I-1)]==x))
+         a <- c(a,i)
+  }
+  return(a)
+}
+y <- c(1,0,0,1,1,1,0,1,1)
+findNum(y,1,2)
